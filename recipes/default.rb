7cf51912222c8368154a3f314e082e91270a2c2f
Florent Vilmart <florentvilmart@me.com>
2015-08-27 22:02:01 -0400

    Fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,13 +5,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-bash 'get_ffmpeg' do
-    cwd "/tmp"
+bash 'default' do
+    cwd '/tmp'
     code <<-EOH
-    wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
-    tar xvf ffmpeg-release-64bit-static.tar.xz
-    cp $(find ./ -name ffmpeg) /usr/local/bin || 0
-    cp $(find ./ -name ffprobe) /usr/local/bin || 0
-    EOH
-    not_if "test -f /usr/local/bin/ffprobe && test -f /usr/local/bin/ffmpeg"
+    
+        wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
+        tar xvf ffmpeg-release-64bit-static.tar.xz
+        cp $(find ./ -name ffmpeg) /usr/local/bin || 0
+        cp $(find ./ -name ffprobe) /usr/local/bin || 0
+        
+        EOH
+    not_if 'test -f /usr/local/bin/ffprobe && test -f /usr/local/bin/ffmpeg'
 end
