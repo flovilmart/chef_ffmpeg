ca561a79a408496f1b1f304a1596ec4e30ef08e0
Florent Vilmart <florentvilmart@me.com>
2015-08-27 21:51:58 -0400

    Fix if existing

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,8 @@ bash 'get_ffmpeg' do
     code <<-EOH
     wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
     tar xvf ffmpeg-release-64bit-static.tar.xz
-    cp $(find ./ -name ffmpeg) /usr/local/bin
-    cp $(find ./ -name ffprobe) /usr/local/bin
+    cp $(find ./ -name ffmpeg) /usr/local/bin || 0
+    cp $(find ./ -name ffprobe) /usr/local/bin || 0
     EOH
+    not_if "test -f /usr/local/bin/ffprobe && test -f /usr/local/bin/ffmpeg"
 end
